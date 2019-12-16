0787f2d4dd0e6594a25e20cedb124c50e971dd66
Jimmy Samanda Rasu <samandajimmyr@gmail.com>
2019-12-16 10:28:45 +0700

    ISG-909 - Goldcard API set up komunikasi request api dengan BRI api

--- models/api_bri.go
+++ models/api_bri.go
@@ -1,8 +1,13 @@
 package models
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/labstack/echo"
 )
@@ -18,6 +23,7 @@ const (
 type APIbri struct {
 	Host         *url.URL
 	API          API
+	Method       string
 	AccessToken  string
 	BRITimestamp string
 	BRISignature string
@@ -38,7 +44,7 @@ func NewBriAPI() (APIbri, error) {
 		return apiBri, err
 	}
 
-	err = apiBri.getAccessToken()
+	err = apiBri.setAccessToken()
 
 	if err != nil {
 		return apiBri, err
@@ -50,18 +56,71 @@ func NewBriAPI() (APIbri, error) {
 	return apiBri, nil
 }
 
-// Request is for blah blah
+// Request represent BRI API Request
 func (bri *APIbri) Request(endpoint string, method string, body interface{}) (*http.Request, error) {
+	bri.Method = method
 	req, err := bri.API.Request(endpoint, method, body)
 
 	if err != nil {
 		return nil, err
 	}
 
+	bri.BRITimestamp = time.Now().UTC().Format(DateTimeFormatZone)
+	err = bri.setBriSignature(endpoint, body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+bri.AccessToken)
+	req.Header.Set("BRI-Signature", bri.BRISignature)
+	req.Header.Set("BRI-Timestamp", bri.BRITimestamp)
+
 	return req, nil
 }
 
-func (bri *APIbri) getAccessToken() error {
+// Do is a function to execute the http request
+func (bri *APIbri) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := bri.API.Do(req, v)
+
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
+}
+
+func (bri *APIbri) setBriSignature(endpoint string, body interface{}) error {
+	var jsonData []byte
+	var bodyStr string
+	jsonData, err := json.Marshal(body)
+
+	if err != nil {
+		return err
+	}
+
+	if string(jsonData) != "null" {
+		bodyStr = string(jsonData)
+	}
+
+	queryParams := url.Values{}
+	queryParams.Set("path", endpoint)
+	queryParams.Set("verb", bri.Method)
+	queryParams.Set("token", "Bearer "+bri.AccessToken)
+	queryParams.Set("timestamp", bri.BRITimestamp)
+	queryParams.Set("body", bodyStr)
+	queryParamStr := bri.getUnorderedURLQuery(queryParams)
+
+	key := []byte(clientSecret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(queryParamStr))
+	sEnc := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	bri.BRISignature = sEnc
+
+	return nil
+}
+
+func (bri *APIbri) setAccessToken() error {
 	response := map[string]interface{}{}
 	params := map[string]string{"client_id": clientID, "client_secret": clientSecret}
 	endpoint := "/oauth/client_credential/accesstoken?grant_type=client_credentials"
@@ -86,4 +145,10 @@ func (bri *APIbri) getAccessToken() error {
 	bri.AccessToken = response["access_token"].(string)
 
 	return nil
+}
+
+func (bri APIbri) getUnorderedURLQuery(queryParams url.Values) string {
+	return "path=" + queryParams.Get("path") + "&verb=" + queryParams.Get("verb") +
+		"&token=" + queryParams.Get("token") + "&timestamp=" + queryParams.Get("timestamp") +
+		"&body=" + queryParams.Get("body")
 }
