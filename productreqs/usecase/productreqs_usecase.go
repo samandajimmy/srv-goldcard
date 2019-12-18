2f1953ff64ffe2a9ac961ed7e6ccc471052d58d5
sean yudhistira <sean.yudhistira@pegadaian.co.id>
2019-12-18 17:45:07 +0700

    Move AddConfigPath from main to productreqs usecase

--- productreqs/usecase/productreqs_usecase.go
+++ productreqs/usecase/productreqs_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"encoding/json"
 	"gade/srv-goldcard/productreqs"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/spf13/viper"
@@ -20,7 +21,7 @@ func ProductReqsUseCase() productreqs.UseCase {
 // ProductRequirements represent to get all product requirements
 func (prodreqs *productreqsUseCase) ProductRequirements(c echo.Context) (map[string]interface{}, error) {
 	val := []byte("")
-
+	viper.AddConfigPath(os.Getenv(`CONFIG_DIR`)) // load all configs
 	viper.SetConfigName("product_requirements")
 	err := viper.ReadInConfig() // Find and read the config file
 
