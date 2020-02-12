c3eb6dd4e0f97fe655cadd1e49ec81eabde8c361
sean yudhistira <sean.yudhistira@pegadaian.co.id>
2020-02-12 16:16:17 +0700

    GCD-8 - GOLDCARD - Add Method on repository interface

--- transactions/repository.go
+++ transactions/repository.go
@@ -11,4 +11,5 @@ type Repository interface {
 	PostBRIPendingTransactions(c echo.Context, trans models.Transaction) error
 	GetAccountByBrixKey(c echo.Context, acc *models.Transaction) error
 	GetTransactionsHistory(c echo.Context, pt models.PayloadHistoryTransactions) ([]models.ResponseHistoryTransactions, error)
+	PostTransactionsANDCardBalance(c echo.Context, trx models.Transaction) error
 }
