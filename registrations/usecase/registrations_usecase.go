c188de6172ad5f2c88ff80da09141ba4bca88935
mochammadnurfaizal <mochammadnurfaizal@pegadaian.co.id>
2020-04-28 08:02:46 +0700

    GCD-378 - GOLDCARD - Goldcard-API Penambahan endpoint reset pengajuan

--- registrations/usecase/registrations_usecase.go
+++ registrations/usecase/registrations_usecase.go
@@ -538,3 +538,21 @@ func (reg *registrationsUseCase) GenerateSlipTEDocument(c echo.Context, acc mode
 
 	return nil
 }
+
+func (reg *registrationsUseCase) ResetRegistration(c echo.Context, pl models.PayloadAppNumber) error {
+	// Get account
+	acc, err := reg.CheckApplication(c, pl)
+
+	if err != nil {
+		return err
+	}
+
+	// Inactive account
+	err = reg.regRepo.InactiveAccount(c, acc)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
