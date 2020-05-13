07e7fcce3febbc51f546a1da18c116bbc37357dd
Jimmy Samanda Rasu <jimmy.rasu@pegadaian.co.id>
2020-05-14 00:08:30 +0700

    BUGFIX - GOLDCARD - change request payload from account number to saving account

--- update_limits/repository.go
+++ update_limits/repository.go
@@ -11,6 +11,7 @@ type Repository interface {
 	GetEmailByKey(c echo.Context) (string, error)
 	GetDocumentByTypeAndApplicationId(appId int64, docType string) (models.Document, error)
 	GetLastLimitUpdate(accId int64) (models.LimitUpdate, error)
+	GetAccountBySavingAccount(c echo.Context, savingAcc string) (models.Account, error)
 }
 
 // Repository represent the update limits Rest Repository
