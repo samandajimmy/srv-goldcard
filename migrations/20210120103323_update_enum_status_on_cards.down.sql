b74f62f6b0a4b963ceea9acc85c11828fef82a3a
shafly-adianto <shafly-adianto@users.noreply.github.com>
2021-01-22 16:46:18 +0700

    GCD-947 - GOLDCARD - Perbaikan migration down untuk perubahan enum status pada cards

--- migrations/20210120103323_update_enum_status_on_cards.down.sql
+++ migrations/20210120103323_update_enum_status_on_cards.down.sql
@@ -1,17 +1,11 @@
 DO $$
 BEGIN
     IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'cards_status_enum') THEN
-        ALTER TYPE cards_status_enum RENAME TO cards_status_enum_old;
-        CREATE TYPE cards_status_enum AS ENUM (
-            'inactive',
-            'active'
-        );
-        
         ALTER TABLE cards
         ALTER COLUMN status DROP DEFAULT,
-        ALTER COLUMN status TYPE cards_status_enum USING status::TEXT::cards_status_enum,
-        ALTER COLUMN status SET DEFAULT 'inactive'::cards_status_enum;
-        DROP TYPE cards_status_enum_old;
+        ALTER COLUMN status TYPE status_enum_default USING status::TEXT::status_enum_default,
+        ALTER COLUMN status SET DEFAULT 'inactive'::status_enum_default;
+        DROP TYPE cards_status_enum;
     END IF;
 END
 $$;
