3a343d052b0979f838135816442d08005456db97
wasis witjaksono <wasiswitjaksono@gmail.com>
2021-04-16 14:22:54 +0700

    GCD-1092 - GOLDCARD - fixing error application status not formatted properly when force deliver

--- registrations/repository/psql_registrations.go
+++ registrations/repository/psql_registrations.go
@@ -668,7 +668,7 @@ func (regis *psqlRegistrationsRepository) GetAppOngoing() ([]models.Account, err
 }
 
 func (regis *psqlRegistrationsRepository) ForceDeliverAccount(c echo.Context, acc models.Account) error {
-	query := `UPDATE applications set status = $1, updated_at = $2 WHERE id = $3;`
+	query := `UPDATE applications set status = $1, card_sent_date = $2, updated_at = $2 WHERE id = $3;`
 	stmt, err := regis.Conn.Prepare(query)
 
 	if err != nil {
